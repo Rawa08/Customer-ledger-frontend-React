ad4a053f084ca7c09118a507de0ae86436e2c5d3
Rawa Aref <rawa_aref@hotmail.com>
2021-11-23 11:44:07 +0100

    Style fix for Tablets

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CustomerList from './components/Customers';
 
 function App() {
   const [customerState, setCustomerState] = useState([]);
+
   const apiKey = process.env.REACT_APP_apiCredentials;
   const sortState = (currentState) => {
     if(!currentState.error){
